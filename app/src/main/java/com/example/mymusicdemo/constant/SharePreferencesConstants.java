2d738bf451ce2e85e0d9b30a8f66840c584a151f
zpf <zpf54670@outlook.com>
2019-06-21 22:03:53 +0800

    update

---
+++ app/src/main/java/com/example/mymusicdemo/constant/SharePreferencesConstants.java
@@ -0,0 +1,7 @@
+package com.example.mymusicdemo.constant;
+
+public class SharePreferencesConstants {
+
+    public static final String SP_NAME_USER = "user";
+    public static final String SP_KEY_PHONE = "phone";
+}
